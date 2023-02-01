eec4b428a97ab18313d149b3f0c39d81737cf5b8
Attaullah Khan <attaullahkhanlodhi@gmail.com>
2023-02-01 13:13:48 +0500

    design ready

---
+++ script.js
@@ -0,0 +1,19 @@
+const ludo_box_1 = document.querySelector('.block_1');
+const ludo_box_2 = document.querySelector('.block_2');
+const ludo_box_3 = document.querySelector('.block_3');
+const ludo_box_4 = document.querySelector('.block_4');
+const ludo_box_5 = document.querySelector('.block_5');
+const ludo_box_6 = document.querySelector('.block_6');
+const ludo_box_7 = document.querySelector('.block_7');
+const ludo_box_8 = document.querySelector('.block_8');
+const ludo_box_9 = document.querySelector('.block_9');
+const ludo_box_10 = document.querySelector('.block_10');
+const ludo_box_11 = document.querySelector('.block_11');
+const ludo_box_12 = document.querySelector('.block_12');
+
+const firstDiceHolder = document.querySelector('.first_dice_holder');
+const secondDiceHolder = document.querySelector('.second_dice_holder');
+
+document.querySelector('.roll-button').addEventListener('click', function(e) {
+    console.log('roll button clicked');
+});
